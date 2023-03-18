c91cfc7a9bcff66ee6997b6e61ed061f26a54a34
yunusemreyakisan <yunusemreyakisan@outlook.com>
2023-03-18 22:37:51 +0300

    Completed

--- app/src/main/java/com/yakisan/depremapi/adapter/DepremAdapter.kt
+++ app/src/main/java/com/yakisan/depremapi/adapter/DepremAdapter.kt
@@ -1,6 +1,5 @@
 package com.yakisan.depremapi.adapter
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,21 +9,22 @@ import com.yakisan.depremapi.R
 import com.yakisan.depremapi.databinding.EarthquakeItemBinding
 import com.yakisan.depremapi.model.DepremModel
 
-class DepremAdapter(val depremler : ArrayList<DepremModel>) : RecyclerView.Adapter<DepremAdapter.VH>(), ClickListener {
-    lateinit var pos : DepremModel
+class DepremAdapter(val depremler: ArrayList<DepremModel>) :
+    RecyclerView.Adapter<DepremAdapter.VH>(), ClickListener {
 
     //ViewHolder
-    class VH(val view : EarthquakeItemBinding) : RecyclerView.ViewHolder(view.root){}
+    class VH(val view: EarthquakeItemBinding) : RecyclerView.ViewHolder(view.root) {}
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
-        val view = DataBindingUtil.inflate<EarthquakeItemBinding>(LayoutInflater.from(parent.context),
-            R.layout.earthquake_item, parent, false)
+        val view = DataBindingUtil.inflate<EarthquakeItemBinding>(
+            LayoutInflater.from(parent.context),
+            R.layout.earthquake_item, parent, false
+        )
         return VH(view)
     }
 
     override fun onBindViewHolder(holder: VH, position: Int) {
-        pos = depremler[position]
-        holder.view.item = pos
+        holder.view.item = depremler[position]
         holder.view.listener = this
     }
 
@@ -33,14 +33,14 @@ class DepremAdapter(val depremler : ArrayList<DepremModel>) : RecyclerView.Adapt
     }
 
     //update list
-    fun updateEarthquakes(liste : ArrayList<DepremModel>){
+    fun updateEarthquakes(liste: ArrayList<DepremModel>) {
         depremler.clear()
         depremler.addAll(liste)
         notifyDataSetChanged()
     }
 
     override fun clicked(view: View) {
-        //Log.e("Click Listener", "Tıklanıldı: $pos")
+
     }
 
 
